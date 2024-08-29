6e9b304928bf2a8ebd6ba91ce5223759879ed9ad
8686300349 <sujakapale6.243@gmail.com>
2024-08-29 20:47:29 +0530

    First commit

---
+++ cv/src/data/resume.tsx
@@ -0,0 +1,211 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Sujal Kapale",
+  initials: "SK",
+  url: "https://dillion.io",
+  location: "San Francisco, CA",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Creative UI/UX Designer with expertise in crafting intuitive and visually engaging digital experiences that enhance user satisfaction.",
+  summary:
+    "I can specify the industries I have experience in, such as tech, e-commerce, restaurant menu , mobile app or any other relevant sectors.I may discuss the ability to adapt the design skills to different industries and user needs.I usually emphasize their core skills, which may include user research, wireframing, prototyping, user testing, and proficiency with design tools like Adobe XD, Figma During my internship at CODSOFT, I had the opportunity to work on a significant project that greatly enhanced my UI/UX design skills.",
+  avatarUrl: "sujal.jpg/",
+  skills: [
+    "Adobe XD",
+    "Figma",
+    "Photoshop",
+    "Adobe Illustrator",
+    "Invision",
+    "Wireframe",
+    "Prototype",
+    "Spline",
+   ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+  ],
+  contact: {
+    email: "sujalkapale60243@gmail.com",
+    tel: "7972507486",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/8686300349",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/sujalkapale/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/kapale_suj39044",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://www.youtube.com/@sujalkapale9374",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Forage",
+      href: "https://www.theforage.com/dashboard",
+      badges: [],
+      location: "Remote",
+      title: "UI/UX Designer",
+      logoUrl: "/theforage_logo.jpeg",
+      start: "Jan 2023",
+      end: "Feb 2023",
+      description:
+        "I completed a virtual internship at Forage in February 2023, where I successfully tackled five diverse tasks. Each task provided hands-on experience in practical industry scenarios, enhancing my skills in problem-solving, time management, and project execution. This internship helped me further develop my professional competencies and prepared me for real-world challenges",
+    },
+    {
+      company: "Dhananjay Enterprises",
+      badges: [],
+      href: "https://www.dhananjayenterprises.com/",
+      location: "Remote",
+      title: "UI/UX Designer",
+      logoUrl: "/d.png",
+      start: "Feb 2024",
+      end: "March 2024",
+      description:
+        "During my internship at Dhananjay Enterprises, I was responsible for designing the company's official website. My role involved creating a user-friendly and visually appealing interface, ensuring seamless navigation, and enhancing the overall user experience. I successfully completed the project, delivering a fully functional and responsive website that met the company's objectives. My internship concluded in March 2024.",
+    },
+  ],
+  education: [
+    {
+      school: "Nutan Maharashtra Institute of Engineering And Technology",
+      href: "https://www.nmiet.edu.in/",
+      degree: "BE(Computer Engineering) ",
+      logoUrl: "/n.png",
+      start: "2021",
+      end: "2025",
+    },
+  ],
+  projects: [
+    {
+      title: "Real-Estate Landingpage",
+      href: "https://www.figma.com/design/nfF7xMCNatgEPciwaMBKn8/Realestate-Website?node-id=0-1&t=fV0ADBWRvW565TmM-1",
+      dates: "Feb 2024 - Mar 2024",
+      active: true,
+      description:
+        "The primary goal of this project is to design an intuitive, user-friendly, and visually appealing landing page for a real estate website. The landing page should provide users with a seamless experience, allowing them to easily search for properties, view listings, and interact with other key features of the site. The design will focus on usability, responsive design across devices, and incorporating the latest UX trends in real estate.",
+      technologies: [
+        "Figma",
+        "Adobe Illustrator",
+        "Photoshop",
+      ],
+      links: [
+        {
+          type: "Drive Link",
+          href: "https://chatcollect.comhttps://drive.google.com/file/d/1HAr11p3w04pDPdqHntrTycRZ816WYvTb/view?usp=drive_link",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/realestate.png",
+      video: ""
+    },
+    {
+      title: "Restaurent Website Landingpage",
+      href: "https://www.figma.com/design/leuGqldq6hbbhfojgSrxup/Restaurant-Home-Page-final?node-id=0-1&t=HPsEpcq4cGHFwmcV-1",
+      dates: "April 2024 - May2024",
+      active: true,
+      description:
+        "I successfully completed the UI/UX design of a restaurant website landing page from April 2024 to May 2024. The design features a seamless user experience, allowing visitors to easily navigate the menu, make reservations, and explore restaurant information. Both desktop and mobile versions were designed to be fully responsive. The project included wireframes, high-fidelity prototypes, and feedback iterations. The final product was delivered on time, providing an engaging and visually appealing user interface.",
+      technologies: [
+        "Figma",
+        "Adobe Illustrator",
+        "Photoshop",
+      ],
+      links: [
+        {
+          type: "Drivelink",
+          href: "https://drive.google.com/file/d/13tl5T3SwkWmubU1Uiw3ooFmgiePcs9cb/view?usp=sharing",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/8686300349/Projects-/blob/main/RealstateWebsite.pdf",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/hotel.png",
+      video: "",
+    },
+    {
+      title: "Travel Agency Website Landingpage",
+      href: "https://www.figma.com/design/mBKJACMsYePLCAvGkAgFWf/Travelling-Website-landingpage?node-id=0-1&t=2M8119nip6pVFp0c-1",
+      dates: "June 2024 - July 2024",
+      active: true,
+      description:
+        "I completed the UI/UX design of a travel agency website landing page from June 2024 to July 2024. The design focuses on providing a user-friendly experience, enabling visitors to easily browse travel packages, book trips, and explore destinations. Responsive layouts for both desktop and mobile were developed. Wireframes, interactive prototypes, and feedback-driven revisions were all part of the process. The final design was delivered on time, ensuring a visually engaging and intuitive interface for users.",
+      technologies: [
+        "Figma",
+        "Adobe Illustrator",
+        "Photoshop",
+      ],
+      links: [
+        {
+          type: "Drivelink",
+          href: "https://drive.google.com/file/d/177JxE8-AXfvFFx5GD6xjr3KTP51iL8jX/view?usp=drive_link",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/8686300349/Projects-/blob/main/Travelling%20website%20landing%20page_compressed.pdf",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/travel.png",
+      video: "",
+    },
+    {
+      title: "Grocery Website Landingpage",
+      href: "https://drive.google.com/file/d/1u0J33-hgy8mDcMtGr277yW7t0-sSQbLm/view?usp=drive_link",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "I completed the UI/UX design for a grocery website landing page between June 2024 and July 2024. The design emphasizes ease of navigation, allowing users to quickly browse products, add items to their cart, and proceed to checkout seamlessly. The project included responsive layouts for both desktop and mobile platforms, along with wireframes and interactive prototypes. Feedback iterations were incorporated to refine the design. The final product was delivered on schedule, providing a smooth and visually appealing shopping experience.",
+      technologies: [
+        "Figma",
+        "Adobe Illustrator",
+        "Photoshop",
+      ],
+      links: [
+        {
+          type: "Drivelink",
+          href: "https://drive.google.com/file/d/1u0J33-hgy8mDcMtGr277yW7t0-sSQbLm/view?usp=drive_link",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/8686300349/Projects-/blob/main/Grocery%20Website.pdf",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/grocery.png",
+      video:"",
+    },
+  ],
+}
+
